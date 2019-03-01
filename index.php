05f1a7779f084443844473963780e21c14422128
Alfred <alfredleo@gmail.com>
2019-03-01 17:39:22 +0500

    Updated failing tests, add grapheme function.

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
+ * Test online with https://3v4l.org/
  * Create function to reverse a string in php
  * Ex: abcd is returned as dcba
  * 1) simplest possible
  * 2) without any built-in functions
  * 3) you can use only one string in memory on low resources.
- * 4) TODO: that works with utf-8 too
+ * 4) that works with utf-8 too
  */
 
 /**
@@ -47,15 +48,52 @@ function reverse3($string)
     return $string;
 }
 
+/**
+ * Pattern modifiers: u(PCRE_UTF8), s(PCRE_DOTALL)
+ * Use utf-8 and match dot.
+ * @param $str
+ * @return string
+ */
+function reverse4($str)
+{
+    preg_match_all('/./us', $str, $ar);
+    return join('', array_reverse($ar[0]));
+}
+
+/**
+ * All other function won't work correct with some grapheme
+ * @param $string
+ * @return string
+ */
+function reverse5($string)
+{
+    $length = grapheme_strlen($string);
+    $ret = [];
+    for ($i = 0; $i < $length; $i += 1) {
+        $ret[] = grapheme_substr($string, $i, 1);
+    }
+    return implode(array_reverse($ret));
+}
+
 // testing
 function testMe($str)
 {
-    echo reverse3($str) . PHP_EOL;
+    echo "--- Testing '$str' ---" . PHP_EOL;
     echo reverse1($str) . PHP_EOL;
-    echo reverse2($str) . PHP_EOL . PHP_EOL;
+    echo reverse2($str) . PHP_EOL;
+    echo reverse3($str) . PHP_EOL;
+    echo reverse4($str) . PHP_EOL;
+    echo reverse5($str) . PHP_EOL . PHP_EOL;
 }
+
 
 testMe('Hello from github');
 testMe('1');
 testMe('ab');
-testMe('тест по UTF8'); // TODO
+testMe('тест по UTF8');
+testMe('اهلا بك');
+testMe('👹👺💀👻');
+// http://www.php.net/manual/en/function.grapheme-strlen.php
+$char_a_ring_nfd = "a\xCC\x8A";  // 'LATIN SMALL LETTER A WITH RING ABOVE' (U+00E5) normalization form "D"
+$char_o_diaeresis_nfd = "o\xCC\x88"; // 'LATIN SMALL LETTER O WITH DIAERESIS' (U+00F6) normalization form "D"
+testMe('abc' . $char_a_ring_nfd . $char_o_diaeresis_nfd);
